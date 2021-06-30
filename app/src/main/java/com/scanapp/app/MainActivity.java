17cf74f14aee4c875aaf4b8e307c492301235ba8
Prashant <veedfrnd@gmail.com>
2021-06-30 15:45:35 +0530

    first commit

---
+++ app/src/main/java/com/scanapp/app/MainActivity.java
@@ -0,0 +1,114 @@
+package com.scanapp.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ErrorCallback;
+import com.budiyev.android.codescanner.ScanMode;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView txtScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scannerView = (CodeScannerView) findViewById(R.id.scannerView);
+        txtScanner = (TextView) findViewById(R.id.txtScanner);
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        codeScanner();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+
+    public void codeScanner() {
+        codeScanner = new CodeScanner(this, scannerView);
+        codeScanner.setCamera(CodeScanner.CAMERA_BACK);
+        codeScanner.setFormats(CodeScanner.ALL_FORMATS);
+        codeScanner.setAutoFocusMode(AutoFocusMode.SAFE);
+        codeScanner.setScanMode(ScanMode.CONTINUOUS);
+        codeScanner.setAutoFocusEnabled(true);
+        codeScanner.setFlashEnabled(false);
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull @org.jetbrains.annotations.NotNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtScanner.setText(result.getText());
+                    }
+                });
+            }
+        });
+        codeScanner.setErrorCallback(new ErrorCallback() {
+            @Override
+            public void onError(@NonNull @NotNull Exception error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtScanner.setText(error.getMessage());
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (codeScanner != null) {
+            codeScanner.startPreview();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (codeScanner != null) {
+            codeScanner.releaseResources();
+        }
+        super.onPause();
+    }
+}
